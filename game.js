c8eeae095e280ee05a6062ce693616e2122da2a4
VVoronkov <voronglass84@gmail.com>
2025-02-05 15:45:57 +0300

    Finish base logic. @//TODO need to fix bugs

--- game.js
+++ game.js
@@ -4,10 +4,13 @@
 // let forward = document.querySelector("line.forward");
 // console.log(forward.getTotalLength());
 
+let timeoutId = null;
+let count = 0;
 let game = document.querySelector(".game");
 let result = document.querySelector(".result");
 let btnGame = document.querySelector(".new-game");
 let fields = document.querySelectorAll(".field");
+
 let step = false;
 let circle = `
             <svg class="circle">
@@ -38,22 +41,87 @@ function init(e) {
 
 function stepCross(data) {
     data.innerHTML = cross;
+    data.classList.add('x');
     const crossSound = new Audio("audio/cross.mp3");
     crossSound.play();
+    count++;
 }
 
 function stepZero(data) {
     data.innerHTML = circle;
+    data.classList.add('o');
     const circleSound = new Audio("audio/zero.mp3");
     circleSound.play();
+    count++;
 }
 
 function newGame() {
-
+    window.location.reload();
 }
 
 function win() {
+    const winCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
 
+    if (fields != null) {
+        for (let i = 0; i < winCombinations.length; i++) {
+            if (fields[winCombinations[i][0]].classList.contains("x") && 
+                fields[winCombinations[i][1]].classList.contains("x") &&
+                fields[winCombinations[i][2]].classList.contains("x")
+                ) {
+                    timeoutId = setTimeout(() => {
+                        fields[winCombinations[i][0]].classList.add("active");
+                        fields[winCombinations[i][1]].classList.add("active");
+                        fields[winCombinations[i][2]].classList.add("active");
+
+                        if (result != null) {
+                            result.innerHTML = "You won with X!";
+                        }
+                    }, 1500);
+
+                    if (game != null) {
+                        game.removeEventListener("click", init);
+                    }
+                }
+            
+            if (fields[winCombinations[i][0]].classList.contains("o") && 
+                fields[winCombinations[i][1]].classList.contains("o") &&
+                fields[winCombinations[i][2]].classList.contains("o")
+                ) {
+                    timeoutId = setTimeout(() => {
+                        fields[winCombinations[i][0]].classList.add("active");
+                        fields[winCombinations[i][1]].classList.add("active");
+                        fields[winCombinations[i][2]].classList.add("active");
+
+                        if (result != null) {
+                            result.innerHTML = "You won with O!";
+                        }
+                    }, 1500);
+
+                    if (game != null) {
+                        game.removeEventListener("click", init);
+                    }
+            }
+
+            if (count === 9) {
+                if (result != null) {
+                    result.innerHTML = "Dead heat";
+                }
+
+                if (game != null) {
+                    game.removeEventListener("click", init);
+                }
+            }
+        }
+    }
 }
 
 if (btnGame != null) {
